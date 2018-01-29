689dbc865133ab4eb2f5c05b67dc961b08608712
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-01-29 16:13:26 +0100

    Adding Roofit kernel estimate to find unbinned fit to sT distribution.

--- makePlot_sT_fromDAS.py
+++ makePlot_sT_fromDAS.py
@@ -8,7 +8,7 @@ from tmProgressBar import tmProgressBar
 
 inputArgumentsParser = argparse.ArgumentParser(description='Run STEALTH selection.')
 inputArgumentsParser.add_argument('--inputFilePath', required=True, help='Path to input file.',type=str)
-inputArgumentsParser.add_argument('--nSTBins', default=4, help='Number of sT bins.',type=int)
+inputArgumentsParser.add_argument('--n_sTBins', default=8, help='Number of sT bins.',type=int)
 inputArgumentsParser.add_argument('--sTMin', default=800., help='Min value of sT.',type=float)
 inputArgumentsParser.add_argument('--sTMax', default=1600., help='Max value of sT.',type=float)
 inputArgumentsParser.add_argument('--sTNormRangeMin', default=800., help='Min value of sT for normalization.',type=float)
@@ -18,7 +18,7 @@ inputArgumentsParser.add_argument('--nJetsNorm', default=2, help='Number of jets
 inputArgumentsParser.add_argument('--outputFilesSuffix', required=True, help='Prefix for output files.',type=str)
 inputArguments = inputArgumentsParser.parse_args()
 
-# rooVar_sT = ROOT.RooRealVar("rooVar_sT", "rooVar_sT", inputArguments.sTMin, inputArguments.sTMax)
+rooVar_sT = ROOT.RooRealVar("rooVar_sT", "rooVar_sT", inputArguments.sTMin, inputArguments.sTMax)
 # rooVar_decayParameter = ROOT.RooRealVar("rooVar_decayParameter", "rooVar_decayParameter", 4.67)
 # rooVar_logTermCoefficient = ROOT.RooRealVar("rooVar_logTermCoefficient", "rooVar_logTermCoefficient", 0.9)
 # rooVar_expCoefficient = ROOT.RooRealVar("rooVar_expCoefficient", "rooVar_expCoefficient", 5.0)
@@ -105,24 +105,24 @@ n_entries = chain_in.GetEntries()
 print ('Total number of events: ' + str(n_entries))
 
 # Initialize histograms and ttrees
-n_st_bins = inputArguments.nSTBins
-st_min = inputArguments.sTMin
-st_max = inputArguments.sTMax
+n_sT_bins = inputArguments.n_sTBins
+sT_min = inputArguments.sTMin
+sT_max = inputArguments.sTMax
 n_jets_min = 2
 n_jets_max = inputArguments.nJetsMax
-file_out = ROOT.TFile('analysis/hSTs_{outputFilesSuffix}.root'.format(outputFilesSuffix=inputArguments.outputFilesSuffix), 'recreate')
+file_out = ROOT.TFile('analysis/h_sTs_{outputFilesSuffix}.root'.format(outputFilesSuffix=inputArguments.outputFilesSuffix), 'recreate')
 histograms = {}
-trees = {}
-sTArrays = {}
 for i in range(n_jets_min, n_jets_max + 1):
-    hist_name = 'st_' + str(i) + 'Jets'
+    hist_name = 'sT_' + str(i) + 'Jets'
     histograms[hist_name] = ROOT.TH1F(
-        'h_' + hist_name, ';S_{T} (GeV);AU', n_st_bins, st_min, st_max
+        'h_' + hist_name, ';S_{T} (GeV);AU', n_sT_bins, sT_min, sT_max
         )
     histograms[hist_name].Sumw2()
-    sTArrays[i] = array.array('f', [0])
-    trees[i] = ROOT.TTree("sTTree_{nJets}Jets".format(nJets=i), "sTTree_{nJets}Jets".format(nJets=i))
-    # trees[i].Branch('rooVar_sT', sTArrays[i], 'rooVar_sT/F')
+
+normTree = ROOT.TTree("normTree", "normTree")
+# norm_sTArray = np.zeros(1, dtype=float)
+norm_sTArray = array.array('f', [0.])
+normTree.Branch('rooVar_sT', norm_sTArray, 'rooVar_sT/F')
 
 # Fill histograms
 progressBar = tmProgressBar(n_entries)
@@ -139,28 +139,33 @@ for j_entry in range(n_entries):
     if (j_entry%progressBarUpdatePeriod == 0): progressBar.updateBar(1.0*j_entry/n_entries, j_entry)
 
     n_stealth_jets = chain_in.b_nJets
-    st = chain_in.b_evtST
-    sTArrays[i] = st
-    trees[i].Fill()
+    sT = chain_in.b_evtST
+    if (n_stealth_jets == inputArguments.nJetsNorm and (sT > sT_min and sT < sT_max)):
+        # print("Storing sT = " + str(sT))
+        norm_sTArray[0] = sT
+        normTree.Fill()
+    i = n_stealth_jets
+    if (i > n_jets_max): i = n_jets_max
+    # trees[i].Fill()
     if n_stealth_jets >= n_jets_min and n_stealth_jets <= n_jets_max:
-        histograms['st_' + str(n_stealth_jets) + 'Jets'].Fill(st)
+        histograms['sT_' + str(n_stealth_jets) + 'Jets'].Fill(sT)
     elif n_stealth_jets >= n_jets_min and n_stealth_jets > n_jets_max:
-        histograms['st_' + str(n_jets_max) + 'Jets'].Fill(st)
+        histograms['sT_' + str(n_jets_max) + 'Jets'].Fill(sT)
 
 print("\n") # proceed to next line after progress bar
 # Scale histograms
 normValues = {}
 for i in range(n_jets_min, n_jets_max + 1):
-    hist_name = 'st_' + str(i) + 'Jets'
+    hist_name = 'sT_' + str(i) + 'Jets'
     norm_bin_start = histograms[hist_name].GetXaxis().FindBin(inputArguments.sTNormRangeMin)
     norm_bin_end = histograms[hist_name].GetXaxis().FindBin(inputArguments.sTNormRangeMax)
     normValues[i] = histograms[hist_name].Integral(norm_bin_start, norm_bin_end)
     histograms[hist_name].Scale(1.0 / normValues[i])
 
 # Plot histograms
-c_st = ROOT.TCanvas('c_st', '', 580, 620)
-c_st.SetBorderSize(0)
-c_st.SetFrameBorderMode(0)
+c_sT = ROOT.TCanvas('c_sT', '', 580, 620)
+c_sT.SetBorderSize(0)
+c_sT.SetFrameBorderMode(0)
 ROOT.gStyle.SetTitleBorderSize(0)
 ROOT.gStyle.SetOptStat(0)
 p_top = ROOT.TPad('p_top', '', 0.005, 0.27, 0.995, 0.995)
@@ -179,30 +184,30 @@ ROOT.gPad.SetLogy()
 line_colors = [
     ROOT.kRed+1, ROOT.kGreen-3, ROOT.kAzure-2, ROOT.kOrange-3, ROOT.kGray
     ]
-hist_name_0 = 'st_' + str(n_jets_min) + 'Jets'
+hist_name_0 = 'sT_' + str(n_jets_min) + 'Jets'
 histograms[hist_name_0].GetYaxis().SetRangeUser(2.0e-4, 9.0)
 histograms[hist_name_0].GetXaxis().SetTitleOffset(1.1)
 histograms[hist_name_0].SetLineColor(line_colors[0])
 histograms[hist_name_0].Draw('E')
 for i in range(n_jets_min + 1, n_jets_max + 1):
-    hist_name = 'st_' + str(i) + 'Jets'
+    hist_name = 'sT_' + str(i) + 'Jets'
     histograms[hist_name].SetLineColor(line_colors[i - n_jets_min])
     histograms[hist_name].Draw('SAME E')
-    c_st.Update()
+    c_sT.Update()
 
 #Draw legend and labels
 legend = ROOT.TLegend(0.75, 0.65, 0.86, 0.92)
 for i in range(n_jets_min, n_jets_max):
-    hist_name = 'st_' + str(i) + 'Jets'
+    hist_name = 'sT_' + str(i) + 'Jets'
     legend.AddEntry(histograms[hist_name], str(i) + ' jets')
-legend.AddEntry(histograms['st_' + str(n_jets_max) + 'Jets'],
+legend.AddEntry(histograms['sT_' + str(n_jets_max) + 'Jets'],
                 '#geq ' + str(n_jets_max) + ' jets')
 legend.SetBorderSize(0)
 legend.Draw()
 
 # Draw ratios
 p_bottom.cd()
-f_unity = ROOT.TF1('f_unity', '[0]', st_min, st_max)
+f_unity = ROOT.TF1('f_unity', '[0]', sT_min, sT_max)
 f_unity.SetTitle("")
 f_unity.SetParameter(0, 1.0)
 f_unity.GetXaxis().SetLabelSize(0.1)
@@ -220,37 +225,34 @@ f_unity.GetYaxis().SetTitleOffset(0.5)
 f_unity.SetLineStyle(4)
 f_unity.Draw()
 graphsToPlot = {}
-hist_name_toCompare = 'st_' + str(inputArguments.nJetsNorm) + 'Jets'
+hist_name_toCompare = 'sT_' + str(inputArguments.nJetsNorm) + 'Jets'
 for i in range(n_jets_min, n_jets_max + 1):
     if (i == inputArguments.nJetsNorm): continue
-    hist_name = 'st_' + str(i) + 'Jets'
+    hist_name = 'sT_' + str(i) + 'Jets'
     graphsToPlot[i] = make_ratio_graph('gae_' + str(i) + 'Jets', histograms[hist_name], histograms[hist_name_toCompare])
     graphsToPlot[i].SetMarkerStyle(20)
     graphsToPlot[i].SetMarkerColor(line_colors[i - n_jets_min])
     graphsToPlot[i].SetLineColor(line_colors[i - n_jets_min])
     graphsToPlot[i].SetMarkerSize(0.9)
     graphsToPlot[i].Draw('P SAME')
-    c_st.Update()
-c_st.SaveAs("analysis/plot_st_{outputFilesSuffix}.png".format(outputFilesSuffix=inputArguments.outputFilesSuffix))
-c_st.Write()
+    c_sT.Update()
+c_sT.SaveAs("analysis/plot_sT_{outputFilesSuffix}.png".format(outputFilesSuffix=inputArguments.outputFilesSuffix))
+c_sT.Write()
 
 ratioTH1Histograms = {}
 chiSqValues = {}
 for i in range(n_jets_min, n_jets_max + 1):
     if (i == inputArguments.nJetsNorm): continue
-    hist_name = 'st_' + str(i) + 'Jets'
-    ratioTH1Histograms[i] = ROOT.TH1F("h_ratio_" + hist_name, ";S_{T} (GeV);AU", n_st_bins, st_min, st_max)
+    hist_name = 'sT_' + str(i) + 'Jets'
+    ratioTH1Histograms[i] = ROOT.TH1F("h_ratio_" + hist_name, ";S_{T} (GeV);AU", n_sT_bins, sT_min, sT_max)
     ratioTH1Histograms[i].Sumw2()
     ratioTH1Histograms[i].Divide(histograms[hist_name], histograms[hist_name_toCompare])
     ratios = []
     ratioErrors = []
-    for stBinCounter in range(1, 1 + n_st_bins):
-        ratios.append(ratioTH1Histograms[i].GetBinContent(stBinCounter))
-        ratioErrors.append(ratioTH1Histograms[i].GetBinError(stBinCounter))
-    chiSqValues[i] = getRatioChiSq(ratios, ratioErrors)/(n_st_bins-1)
-
-file_out.Write()
-file_out.Close()
+    for sTBinCounter in range(1, 1 + n_sT_bins):
+        ratios.append(ratioTH1Histograms[i].GetBinContent(sTBinCounter))
+        ratioErrors.append(ratioTH1Histograms[i].GetBinError(sTBinCounter))
+    chiSqValues[i] = getRatioChiSq(ratios, ratioErrors)/(n_sT_bins-1)
 
 chiSqFile = open("analysis/chiSqRatios_{outputFilesSuffix}.txt".format(outputFilesSuffix=inputArguments.outputFilesSuffix), 'w')
 for i in range(n_jets_min, n_jets_max + 1):
@@ -263,7 +265,26 @@ for i in range(n_jets_min, n_jets_max + 1):
     normValuesFile.write(str(i) + "    " + str(normValues[inputArguments.nJetsNorm]/normValues[i]) + '\n')
 normValuesFile.close()
 
-# unbinnedRooSet = ROOT.RooDataSet("inputData", "inputData", trees[inputArguments.nJetsNorm], rooVar_sT)
+unbinnedRooSet = ROOT.RooDataSet("inputData", "inputData", normTree, ROOT.RooArgSet(rooVar_sT))
+kernels = {"NoMirror": ROOT.RooKeysPdf.NoMirror, "MirrorLeft": ROOT.RooKeysPdf.MirrorLeft, "MirrorRight": ROOT.RooKeysPdf.MirrorRight, "MirrorBoth": ROOT.RooKeysPdf.MirrorBoth, "MirrorAsymLeft": ROOT.RooKeysPdf.MirrorAsymLeft, "MirrorAsymLeftRight": ROOT.RooKeysPdf.MirrorAsymLeftRight, "MirrorAsymRight": ROOT.RooKeysPdf.MirrorAsymRight, "MirrorLeftAsymRight": ROOT.RooKeysPdf.MirrorLeftAsymRight, "MirrorAsymBoth": ROOT.RooKeysPdf.MirrorAsymBoth}
+rooKernelFunctions = {}
+for kernelType in kernels.keys():
+    print("Defining fit for kernel type: " + kernelType)
+    rooKernelFunctions[kernelType] = ROOT.RooKeysPdf("rooKernelFunction_" + kernelType, "rooKernelFunction_" + kernelType, rooVar_sT, unbinnedRooSet, kernels[kernelType])
+    sTFrame = rooVar_sT.frame(sT_min, sT_max, inputArguments.n_sTBins)
+    unbinnedRooSet.plotOn(sTFrame)
+    rooKernelFunctions[kernelType].plotOn(sTFrame)
+    c_sTUnbinnedFit = ROOT.TCanvas('c_sTUnbinnedFit_' + kernelType, 'c_sTUnbinnedFit_' + kernelType, 1024, 768)
+    c_sTUnbinnedFit.SetBorderSize(0)
+    c_sTUnbinnedFit.SetFrameBorderMode(0)
+    sTFrame.Draw()
+    c_sTUnbinnedFit.SaveAs("analysis/plot_sT_UnbinnedFit_{outputFilesSuffix}_{kernelType}.png".format(outputFilesSuffix=inputArguments.outputFilesSuffix, kernelType=kernelType))
+    c_sTUnbinnedFit.Write()
+
+normTree.Write()
+file_out.Write()
+file_out.Close()
+
 # for sTBackgroundFunctionName in sTBackgroundFunctionNames:
 #     rooPDFs[sTBackgroundFunctionName].fitTo(unbinnedRooHist)
 
