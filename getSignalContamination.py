b35a012b9a443865a01192374580c9c54090f1e3
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-03-14 00:25:42 +0100

    Added final step to signal contamination calculation: added a histogram of the weighted number of events, weighing each MC event correctly, and a histogram of the signal contamination in the input sample. Also added file containing cross sections as a function of gluino mass, needed as input for calculating the weight for a given MC event.

--- getSignalContamination.py
+++ getSignalContamination.py
@@ -10,17 +10,20 @@ from tmGeneralUtils import prettyPrintDictionary
 inputArgumentsParser = argparse.ArgumentParser(description='Get signal contamination from input MC in real data.')
 inputArgumentsParser.add_argument('--inputMCPath', required=True, help='Path to input MC file.',type=str)
 inputArgumentsParser.add_argument('--inputDataPath', required=True, help='Path to input data file.',type=str)
+inputArgumentsParser.add_argument('--crossSectionsFile', default="SusyCrossSections13TevGluGlu.txt", help='Path to dat file that contains cross-sections as a function of gluino mass, to use while weighting events.',type=str)
 inputArgumentsParser.add_argument('--sTMin', default=1100., help='Min value of sT.',type=float)
 inputArgumentsParser.add_argument('--outputPrefix', required=True, help='Prefix to output files.',type=str)
 inputArgumentsParser.add_argument('--nJetsMin', default=2, help='Minimum number of jets in event.',type=str)
 inputArgumentsParser.add_argument('--nJetsMax', default=6, help='Least value of nJets in highest nJets bin.',type=str)
 inputArgumentsParser.add_argument('--nJets', action='append', help='nJets to plot.',type=str)
-inputArgumentsParser.add_argument('--nGluinoMassBins', default=20, help='nBins on the gluino mass axis.',type=int) # 775 GeV --> 1775 GeV in steps of 50 GeV
+inputArgumentsParser.add_argument('--nGluinoMassBins', default=20, help='nBins on the gluino mass axis.',type=int) # (800 - 25) GeV --> (1750 + 25) GeV in steps of 50 GeV
 inputArgumentsParser.add_argument('--minGluinoMass', default=775., help='Min gluino mass.',type=float)
 inputArgumentsParser.add_argument('--maxGluinoMass', default=1775., help='Max gluino mass.',type=float)
 inputArgumentsParser.add_argument('--nNeutralinoMassBins', default=133, help='nBins on the neutralino mass axis.',type=int)
 inputArgumentsParser.add_argument('--minNeutralinoMass', default=93.75, help='Min neutralino mass.',type=float)
-inputArgumentsParser.add_argument('--maxNeutralinoMass', default=1756.25, help='Max neutralino mass.',type=float)
+inputArgumentsParser.add_argument('--maxNeutralinoMass', default=1756.25, help='Max neutralino mass.',type=float) # (100 - 6.25) GeV --> (1750 + 6.25) GeV in steps of 12.5 GeV
+inputArgumentsParser.add_argument('--totalIntegratedLuminosity', default=37760., help='Max neutralino mass.',type=float) # total integrated luminosity in 2016 = 37.76 inverse femtobarns
+inputArgumentsParser.add_argument('--nGeneratedEventsPerBin', default=150000, help='Number of generated events per bin in the MC samples.',type=int)
 inputArguments = inputArgumentsParser.parse_args()
 
 MCPIDs = {
@@ -28,6 +31,18 @@ MCPIDs = {
     "gluino": 1000021,
     "neutralino": 1000022
 }
+
+crossSectionsInputFileObject = open(inputArguments.crossSectionsFile, 'r')
+crossSectionsDictionary = {}
+for line in crossSectionsInputFileObject:
+    crossSectionsData = line.split()
+    gluinoMass = int(0.5 + float(crossSectionsData[0]))
+    crossSection = float(crossSectionsData[1])
+    crossSectionsDictionary[gluinoMass] = crossSection
+crossSectionsInputFileObject.close()
+
+print("Read in cross-sections as a function of gluino mass:")
+prettyPrintDictionary(crossSectionsDictionary)
 
 def getGeneratedMasses(inputMCChain):
     generatedMasses = {"gluino": 0., "neutralino": 0.}
@@ -44,6 +59,13 @@ def getGeneratedMasses(inputMCChain):
     if not(gluinoMassIsSet): sys.exit("Unable to find gluino mass in an event!")
     if not(neutralinoMassIsSet): sys.exit("Unable to find neutralino mass in an event!")
     return generatedMasses
+
+def getWeight(gluinoMass, neutralinoMass):
+    try:
+        weight = crossSectionsDictionary[int(0.5 + gluinoMass)]*inputArguments.totalIntegratedLuminosity/inputArguments.nGeneratedEventsPerBin
+    except KeyError:
+        sys.exit("Unable to find cross-section corresponding to following gluino mass: {mass}".format(mass=gluinoMass))
+    return weight
 
 sw = ROOT.TStopwatch()
 sw.Start()
@@ -84,16 +106,20 @@ print("Analyzing MC sample...")
 inputMCChain = ROOT.TChain('ggNtuplizer/EventTree')
 inputMCChain.Add(inputArguments.inputMCPath)
 nMCEntries = inputMCChain.GetEntries()
-print ("Total number of available events in MC: {nMCEntries}".format(nMCEntries=nMCEntries))
+print ("Total number of available events in MC samples: {nMCEntries}".format(nMCEntries=nMCEntries))
 
 histograms_total_nMCEvents = {}
+histograms_weighted_nMCEvents = {}
+histograms_signalContamination = {}
 for nJets in range(inputArguments.nJetsMin, 1 + inputArguments.nJetsMax):
     histograms_total_nMCEvents[nJets] = ROOT.TH2F("h_total_nMCEvents_{nJets}Jets".format(nJets=nJets), "h_total_nMCEvents_{nJets}Jets".format(nJets=nJets), inputArguments.nGluinoMassBins, inputArguments.minGluinoMass, inputArguments.maxGluinoMass, inputArguments.nNeutralinoMassBins, inputArguments.minNeutralinoMass, inputArguments.maxNeutralinoMass)
+    histograms_weighted_nMCEvents[nJets] = ROOT.TH2F("h_weighted_nMCEvents_{nJets}Jets".format(nJets=nJets), "h_weighted_nMCEvents_{nJets}Jets".format(nJets=nJets), inputArguments.nGluinoMassBins, inputArguments.minGluinoMass, inputArguments.maxGluinoMass, inputArguments.nNeutralinoMassBins, inputArguments.minNeutralinoMass, inputArguments.maxNeutralinoMass)
+    histograms_signalContamination[nJets] = ROOT.TH2F("h_signalContamination_{nJets}Jets".format(nJets=nJets), "h_signalContamination_{nJets}Jets".format(nJets=nJets), inputArguments.nGluinoMassBins, inputArguments.minGluinoMass, inputArguments.maxGluinoMass, inputArguments.nNeutralinoMassBins, inputArguments.minNeutralinoMass, inputArguments.maxNeutralinoMass)
 
 progressBar = tmProgressBar(nMCEntries)
 progressBarUpdatePeriod = max(1, nMCEntries//1000)
 progressBar.initializeTimer()
-for entryIndex in range(10000):
+for entryIndex in range(nMCEntries):
     entryStatus = inputMCChain.LoadTree(entryIndex)
     if entryStatus < 0: sys.exit("Tree failed to load entry at index {entryIndex}; returned status {entryStatus}".format(entryIndex=entryIndex, entryStatus=entryStatus))
     chainStatus = inputMCChain.GetEntry(entryIndex)
@@ -106,10 +132,17 @@ for entryIndex in range(10000):
     if (nJetsBin > inputArguments.nJetsMax): nJetsBin = inputArguments.nJetsMax
     if (nJetsBin < inputArguments.nJetsMin): sys.exit("Unexpected nJetsBin = {nJetsBin} at entry index = {entryIndex}".format(nJetsBin=nJetsBin, entryIndex=entryIndex))
     sT = inputMCChain.b_evtST
-    generatedMasses = getGeneratedMasses(inputMCChain)
     if (sT > inputArguments.sTMin):
-        histograms_total_nMCEvents[nJetsBin].Fill(generatedMasses["gluino"], generatedMasses["neutralino"], 1./1000)
+        generatedMasses = getGeneratedMasses(inputMCChain)
+        generated_gluinoMass = generatedMasses["gluino"]
+        generated_neutralinoMass = generatedMasses["neutralino"]
+        eventWeight = getWeight(generatedMasses["gluino"], generatedMasses["neutralino"])
+        histograms_total_nMCEvents[nJetsBin].Fill(generated_gluinoMass, generated_neutralinoMass, 1.)
+        histograms_weighted_nMCEvents[nJetsBin].Fill(generated_gluinoMass, generated_neutralinoMass, eventWeight)
+        histograms_signalContamination[nJetsBin].Fill(generated_gluinoMass, generated_neutralinoMass, eventWeight/nEventsInData[nJetsBin])
 progressBar.terminate()
 
 for nJets in range(inputArguments.nJetsMin, 1 + inputArguments.nJetsMax):
-    tmROOTUtils.plotObjectsOnCanvas(listOfObjects = [histograms_total_nMCEvents[nJets]], canvasName = "c_total_nMCEvents_{nJets}Jets".format(nJets=nJets), outputDocumentName = "analysis/signalContamination/{outputPrefix}_{nJets}Jets".format(outputPrefix=inputArguments.outputPrefix, nJets=nJets), customOptStat=0, customPlotOptions_firstObject="TEXTCOLZ")
+    tmROOTUtils.plotObjectsOnCanvas(listOfObjects = [histograms_total_nMCEvents[nJets]], canvasName = "c_total_nMCEvents_{nJets}Jets".format(nJets=nJets), outputDocumentName = "analysis/signalContamination/{outputPrefix}_total_nEvents_{nJets}Jets".format(outputPrefix=inputArguments.outputPrefix, nJets=nJets), customOptStat=0, customPlotOptions_firstObject="TEXTCOLZ")
+    tmROOTUtils.plotObjectsOnCanvas(listOfObjects = [histograms_weighted_nMCEvents[nJets]], canvasName = "c_weighted_nMCEvents_{nJets}Jets".format(nJets=nJets), outputDocumentName = "analysis/signalContamination/{outputPrefix}_weighted_nEvents_{nJets}Jets".format(outputPrefix=inputArguments.outputPrefix, nJets=nJets), customOptStat=0, customPlotOptions_firstObject="TEXTCOLZ")
+    tmROOTUtils.plotObjectsOnCanvas(listOfObjects = [histograms_signalContamination[nJets]], canvasName = "c_signalContamination_{nJets}Jets".format(nJets=nJets), outputDocumentName = "analysis/signalContamination/{outputPrefix}_signalContamination_{nJets}Jets".format(outputPrefix=inputArguments.outputPrefix, nJets=nJets), customOptStat=0, customPlotOptions_firstObject="COLZ")
