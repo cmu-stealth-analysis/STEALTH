33e5ea3f4ea2e64e3f06f8e18b6af9322711f0ed
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-06-04 21:50:30 +0200

    Added option to vary value of bit max value while plotting HLT photon bits.

--- plotHLTPhotonBits.py
+++ plotHLTPhotonBits.py
@@ -11,6 +11,7 @@ inputArgumentsParser.add_argument('--inputFromFile', action='store_true', help="
 inputArgumentsParser.add_argument('--inputFilePath', required=True, help='Path to input file.',type=str)
 inputArgumentsParser.add_argument('--outputFilePrefix', required=True, help='Prefix for output file.',type=str)
 inputArgumentsParser.add_argument('--maxNEvents', default=-1, help='Maximum number of events to process.',type=int)
+inputArgumentsParser.add_argument('--bitMaxValue', default=35, help='Maximum value of photon bit up to which to plot.', type=int)
 inputArguments = inputArgumentsParser.parse_args()
 
 def passesHLTBit(inputChainObject=None, bitIndex=None):
@@ -39,7 +40,7 @@ nEventsToProcess = 0
 if (inputArguments.maxNEvents > 0 and inputArguments.maxNEvents < nEvents): nEventsToProcess = inputArguments.maxNEvents
 else: nEventsToProcess = nEvents
 
-h_HLTBitsHistogram = ROOT.TH1F("h_HLTBitsHistogram", "HLT Bits;Bit Index;A.U.", 31,-0.5, 30.5)
+h_HLTBitsHistogram = ROOT.TH1F("h_HLTBitsHistogram", "HLT Bits;Bit Index;A.U.", 1 + inputArguments.bitMaxValue, -0.5, 0.5+inputArguments.bitMaxValue)
 
 progressBar = tmProgressBar(nEventsToProcess)
 progressBarUpdatePeriod = 1+(nEventsToProcess//1000)
@@ -56,7 +57,7 @@ for eventIndex in range(0,nEventsToProcess):
         # sys.exit("Event in tree unreadable.")
         continue
     if (eventIndex%progressBarUpdatePeriod == 0 or eventIndex == (nEventsToProcess - 1)): progressBar.updateBar(eventIndex/nEventsToProcess, eventIndex)
-    for bitIndex in range(0, 31):
+    for bitIndex in range(0, 1+inputArguments.bitMaxValue):
         if (passesHLTBit(inputTreeObject, bitIndex)): h_HLTBitsHistogram.Fill(bitIndex)
 
 progressBar.terminate()
