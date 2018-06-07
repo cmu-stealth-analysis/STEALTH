9c2bf589fcbd7ba28c089e949b2d53897a2424c7
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-06-07 16:35:49 +0200

    Removed a few unnecessary files.

--- appendString.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Silly little script that adds a common prefix to all lines in a file; useful for creating input file lists that start with the standard "root://cmseos.fnal.gov//store/..." prefix
-
-sed -e "s|^|${1}|" ${2} > ${2}.tmp
-mv ${2}.tmp ${2}
