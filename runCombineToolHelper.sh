34f83f2e129fe1faf4b02eb94642e6ccd8ec88d1
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-06-12 18:16:24 +0200

    Enabled setting prefix of data cards in combine tool.

--- runCombineToolHelper.sh
+++ runCombineToolHelper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running combine tool for gluino mass ${2}, neutralino mass ${3}."
+echo "Running combine tool for gluino mass ${3}, neutralino mass ${4}."
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc6_amd64_gcc630
 cd /uscms/homes/t/tmudholk/private/stealth/cmssw/CMSSW_9_4_4/src && eval `scramv1 runtime -sh` && cd /uscms/home/tmudholk/private/stealth/STEALTH/${1}
@@ -8,8 +8,8 @@ echo "CMSSW version: "$CMSSW_BASE
 echo "combine tool path:"
 which combine | cat
 
-combine -M AsymptoticLimits dataCard_gluinoMass_${2}_neutralinoMass_${3}.txt -n "_gluinoMass_${2}_neutralinoMass_${3}"
-rsync --progress -av higgsCombine_gluinoMass_${2}_neutralinoMass_${3}.* /uscms/home/tmudholk/private/stealth/STEALTH/${4}/ && rm higgsCombine_gluinoMass_${2}_neutralinoMass_${3}.*
+combine -M AsymptoticLimits dataCard_${2}_gluinoMass_${3}_neutralinoMass_${4}.txt -n "_${2}_gluinoMass_${3}_neutralinoMass_${4}"
+rsync --progress -av higgsCombine_${2}_gluinoMass_${3}_neutralinoMass_${4}.* /uscms/home/tmudholk/private/stealth/STEALTH/${5}/ && rm higgsCombine_${2}_gluinoMass_${3}_neutralinoMass_${4}.*
 echo "Resourcing old environment..."
 cd /uscms/home/tmudholk/private/stealth/STEALTH && source setupEnv.sh
-echo "combine tool ran successfully for gluino mass ${2}, neutralino mass ${3}."
+echo "combine tool ran successfully for gluino mass ${3}, neutralino mass ${4}."
