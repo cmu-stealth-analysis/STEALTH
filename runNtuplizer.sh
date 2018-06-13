180823b4a2182f0a88d3abb273bc4432be42e48d
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-06-13 01:02:06 -0500

    Removed obsolete scripts.

--- runNtuplizer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WORKDIR=/afs/cern.ch/user/m/mandrews/work/STEALTH
-#ggFILE=SinglePhoton_2016B_evtSt.root
-
-python $WORKDIR/getST.py
-
-cp * $WORKDIR/ggNTUPLES/
-
--- submitJobs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WORKDIR=/afs/cern.ch/user/m/mandrews/work/STEALTH
-
-#bsub -J Ntuplizer -o $WORKDIR/logs/stdout.log -e $WORKDIR/logs/stderr.log -q 8nh < $WORKDIR/runNtuplizer.sh
-bsub -q 1nd < $WORKDIR/runNtuplizer.sh
-#bsub -q 1nw < $WORKDIR/runNtuplizer.sh
