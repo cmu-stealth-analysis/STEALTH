5f0bc5fce362edf98694e70901f391af89e64442
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-07-06 16:56:44 +0200

    Minor changes to setupEnv for compatibility with lxplus.

--- setupEnv.sh
+++ setupEnv.sh
@@ -1,10 +1,12 @@
+set -x
+
 BASEHOSTNAME=$(echo $HOSTNAME | sed "s|\([^\.]*\)\.cern\.ch|\1|" | sed "s|\([^\.]*\)\.fnal\.gov|\1|")
 
 if [[ "$BASEHOSTNAME" =~ ^lxplus[0-9]{3,4}$ ]]; then
     WORKDIR="/afs/cern.ch/user/t/tmudholk/public/research/stealth/from_michael/STEALTH"
     export X509_USER_PROXY=/afs/cern.ch/user/t/tmudholk/private/x509up_u83667
     source /cvmfs/cms.cern.ch/cmsset_default.sh
-    cd /afs/cern.ch/user/t/tmudholk/public/research/ecaldqm/cmssw/CMSSW_9_2_13/src && eval `scramv1 runtime -sh`
+    cd /afs/cern.ch/user/t/tmudholk/public/research/stealth/from_michael/StealthProduction/CMSSW_9_4_4/src && eval `scramv1 runtime -sh`
     export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH
     cd ${WORKDIR}
 elif [[ "$BASEHOSTNAME" =~ ^cmslpc[0-9]{2}$ ]]; then
@@ -14,4 +16,8 @@ elif [[ "$BASEHOSTNAME" =~ ^cmslpc[0-9]{2}$ ]]; then
     cd /uscms/home/tmudholk/private/stealth/cmssw/CMSSW_9_2_13/src && eval `scramv1 runtime -sh`
     export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH
     cd ${WORKDIR}
+else
+    echo "Sourcing env only supported for lxplus and FNAL nodes at the moment."
 fi
+
+set +x
