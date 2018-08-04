a7bfa24b7ca8b2e4fd054edb870c8a9bf9b0ab5e
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-08-04 11:42:48 +0200

    Reduced printout from setupEnv.sh for more readability.

--- setupEnv.sh
+++ setupEnv.sh
@@ -5,16 +5,16 @@ BASEHOSTNAME=$(echo $HOSTNAME | sed "s|\([^\.]*\)\.cern\.ch|\1|" | sed "s|\([^\.
 if [[ "$BASEHOSTNAME" =~ ^lxplus[0-9]{3,4}$ ]]; then
     WORKDIR="/afs/cern.ch/user/t/tmudholk/public/research/stealth/from_michael/STEALTH"
     export X509_USER_PROXY=/afs/cern.ch/user/t/tmudholk/private/x509up_u83667
-    source /cvmfs/cms.cern.ch/cmsset_default.sh
-    cd /afs/cern.ch/user/t/tmudholk/public/research/stealth/from_michael/StealthProduction/CMSSW_9_4_4/src && eval `scramv1 runtime -sh`
-    export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH
+    set +x && source /cvmfs/cms.cern.ch/cmsset_default.sh && set -x
+    cd /afs/cern.ch/user/t/tmudholk/public/research/stealth/from_michael/StealthProduction/CMSSW_9_4_4/src && set +x && eval `scramv1 runtime -sh` && export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH && set -x
+    echo "Set default CMSSW_BASE = ${CMSSW_BASE}"
     cd ${WORKDIR}
 elif [[ "$BASEHOSTNAME" =~ ^cmslpc[0-9]{2}$ ]]; then
     WORKDIR="/uscms/home/tmudholk/private/stealth/STEALTH"
     export X509_USER_PROXY=/uscms/home/tmudholk/private/x509up_u50838
-    source /cvmfs/cms.cern.ch/cmsset_default.sh
-    cd /uscms/home/tmudholk/private/stealth/cmssw/CMSSW_9_2_13/src && eval `scramv1 runtime -sh`
-    export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH
+    set +x && source /cvmfs/cms.cern.ch/cmsset_default.sh && set -x
+    cd /uscms/home/tmudholk/private/stealth/cmssw/CMSSW_9_2_13/src && set +x && eval `scramv1 runtime -sh` && export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH && set -x
+    echo "Set default CMSSW_BASE = ${CMSSW_BASE}"
     cd ${WORKDIR}
 else
     echo "Sourcing env only supported for lxplus and FNAL nodes at the moment."
