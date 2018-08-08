8ad9d21b18624b6c843e06d0767847a15dc977bb
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-08-08 16:55:33 +0200

    Second commit, many new features in event selection C++ code, including the storing of all passed options and hard-coded parameters in the output ROOT file and a new executable to print them from an output generated this way."

--- eventSelection/include/runEventSelection.h
+++ eventSelection/include/runEventSelection.h
@@ -10,54 +10,53 @@
 #include "tmArgumentParser.h"
 #include "tmProgressBar.h"
 #include "tmMiscellaneous.h"
-#include "tmROOTSaverUtils.h"
+/* #include "tmROOTSaverUtils.h" */
 #include "TROOT.h"
 #include "TTree.h"
 /* #include "TTreeReader.h" */
 /* #include "TTreeReaderValue.h" */
 /* #include "TTreeReaderArray.h" */
-/* #include "TFile.h" */
-/* #include "TH1F.h" */
-/* #include "TH2F.h" */
-/* #include "TObjArray.h" */
-/* #include "TLegend.h" */
-/* #include "TLegendEntry.h" */
-
-#define MCPID_PHOTON 22
-#define MCPID_GLUINO 1000021
-#define MCPID_NEUTRALINO 1000022
+#include "TFile.h"
+#include "TNamed.h"
 
 struct PIDsStruct {
   const int photon = 22;
   const int gluino = 1000021;
   const int neutralino = 1000022;
+  friend std::ostream& operator<< (std::ostream& out, const PIDsStruct& PIDs) {
+    out << "photon --> " << PIDs.photon << ", "
+        << "gluino --> " << PIDs.gluino << ", "
+        << "neutralino --> " << PIDs.neutralino;
+    return out;
+  }
 };
 
 struct rangeStruct{
   float rangeLower, rangeUpper;
 rangeStruct(float rangeLower_, float rangeUpper_) : rangeLower(rangeLower_),
     rangeUpper(rangeUpper_){}
-  bool isInRange(float candidate) {
+  bool isInside(float candidate) {
     return (candidate >= rangeLower && candidate < rangeUpper);
   }
-};
-
-struct neutralIsolationCutStruct{
-  float constCoefficient, linearCoefficient, squareCoefficient;
-neutralIsolationCutStruct(float constCoefficient_, float linearCoefficient_, float squareCoefficient_) : constCoefficient(constCoefficient_),
-    linearCoefficient(linearCoefficient_),
-    squareCoefficient(squareCoefficient_) {}
-  float polynomialValue(float pT) {
-    return (constCoefficient + linearCoefficient*pT + squareCoefficient*pT*pT);
+  friend std::ostream& operator<< (std::ostream& out, const rangeStruct& range) {
+    out << "[" << range.rangeLower << ", " << range.rangeUpper << ")";
+    return out;
   }
 };
 
-struct photonIsolationCutStruct{
-  float constCoefficient, linearCoefficient;
-photonIsolationCutStruct(float constCoefficient_, float linearCoefficient_) : constCoefficient(constCoefficient_),
-    linearCoefficient(linearCoefficient_) {}
-  float polynomialValue(float pT) {
-    return (constCoefficient + linearCoefficient*pT);
+struct quadraticPolynomialStruct{
+  float constCoefficient, linearCoefficient, squareCoefficient;
+quadraticPolynomialStruct(float constCoefficient_, float linearCoefficient_, float squareCoefficient_) : constCoefficient(constCoefficient_),
+    linearCoefficient(linearCoefficient_),
+    squareCoefficient(squareCoefficient_) {}
+  float getPolynomialValue(float pT) {
+    return (constCoefficient + linearCoefficient*pT + squareCoefficient*pT*pT);
+  }
+  friend std::ostream& operator<< (std::ostream& out, const quadraticPolynomialStruct& polynomial) {
+    out << "const --> " << polynomial.constCoefficient << ", "
+        << "linear--> " << polynomial.linearCoefficient << ", "
+        << "square --> " << polynomial.squareCoefficient;
+    return out;
   }
 };
 
@@ -67,6 +66,14 @@ EAValuesStruct(float regionUpperBound_, float chargedHadronsEA_, float neutralHa
     chargedHadronsEA(chargedHadronsEA_),
     neutralHadronsEA(neutralHadronsEA_),
     photonsEA(photonsEA_) {}
+
+  friend std::ostream& operator<< (std::ostream& out, const EAValuesStruct& EAValues) {
+    out << "region upper eta bound --> " << EAValues.regionUpperBound << ", effective areas: "
+        << "charged hadrons --> " << EAValues.chargedHadronsEA << ", "
+        << "neutral hadrons --> " << EAValues.neutralHadronsEA << ", "
+        << "photons --> " << EAValues.photonsEA;
+    return out;
+  }
 };
 
 struct parametersStruct {
@@ -97,21 +104,156 @@ struct parametersStruct {
   const PIDsStruct PIDs;
   const int nPhotonsWithNeutralinoMom = 2;
   const float towerHOverECut = 0.035;
-  const rangeStruct sigmeietaietaRange = rangeStruct(0.0103, 0.015);
+  const rangeStruct sigmaietaietaRange = rangeStruct(0.0103, 0.015);
   const rangeStruct chargedIsolationRange = rangeStruct(1.416, 15.0);
-  const neutralIsolationCutStruct neutralIsolationCut = neutralIsolationCutStruct(2.491, 0.0126, 0.000026);
-  const photonIsolationCutStruct photonIsolationCut = photonIsolationCutStruct(2.952, 0.004);
+  const quadraticPolynomialStruct neutralIsolationCut = quadraticPolynomialStruct(2.491, 0.0126, 0.000026);
+  const quadraticPolynomialStruct photonIsolationCut = quadraticPolynomialStruct(2.952, 0.004, 0.0);
   int HLTPhotonBit = -1;
-  const int HLTPhotonBit2016 = -1;
-  const int HLTPhotonBit2017 = -1;
-  void setHLTPhotonBitForYear(int year) {
+  const int HLTPhotonBit2016 = 14;
+  const int HLTPhotonBit2017 = 36;
+  void tuneParametersForYear(int year) {
     if (year == 2017) HLTPhotonBit = HLTPhotonBit2017;
     else if (year == 2016) HLTPhotonBit = HLTPhotonBit2016;
   }
+  friend std::ostream& operator<< (std::ostream& out, const parametersStruct& parameters) {
+    out << "PIDs: " << parameters.PIDs << std::endl;
+
+    out << "Photon cuts:" << std::endl
+        << "pT_SubLeading: " << parameters.pTCutSubLeading << ", "
+        << "pT_Leading: " << parameters.pTCutLeading << ", "
+        << "eta: " << parameters.photonEtaCut << ", "
+        << "R9: " << parameters.R9Cut << ", "
+        << "HOverE: " << parameters.towerHOverECut << ", "
+        << "sigmaietaietaRange: " << parameters.sigmaietaietaRange << ", "
+        << "chargedIsolationRange: " << parameters.chargedIsolationRange << ", "
+        << "neutral isolation cut coefficients: " << parameters.neutralIsolationCut << ", "
+        << "photon isolation cut coefficients: " << parameters.photonIsolationCut << std::endl;
+
+    out << "Region 1 effective areas: " << parameters.region1EAs << std::endl
+        << "Region 2 effective areas: " << parameters.region2EAs << std::endl;
+
+    out << "Jet cuts:" << std::endl
+        << "pT: " << parameters.jetpTCut << ", "
+        << "eta: " << parameters.jetEtaCut << ", "
+        << "PUID: " << parameters.jetPUIDThreshold << ", "
+        << "jet ID: " << parameters.jetSelectionID << ", "
+        << "minDeltaR: " << parameters.minDeltaRCut << std::endl;
+
+    out << "Electron cuts:" << std::endl
+        << "pT: " << parameters.electronPtCut << ", "
+        << "eta: " << parameters.electronEtaCut << ", "
+        << "Dz: " << parameters.electronDzCut << ", "
+        << "PFPUIso: " << parameters.electronPFPUIsoCut << std::endl;
+
+    out << "Muon cuts:" << std::endl
+        << "pT: " << parameters.muonPtCut << ", "
+        << "PFPUIso: " << parameters.muonPFPUIsoCut << std::endl;
+
+    out << "MET cuts:" << std::endl
+        << "MET: " << parameters.METThreshold << std::endl;
+
+    out << "Event cuts:" << std::endl
+        << "nSubLeading photons: " << parameters.nSubLeadingPhotons << ", "
+        << "nLeading photons min: " << parameters.nLeadingPhotons << ", "
+        << "photon HLT bit index: " << parameters.HLTPhotonBit << ", "
+        << "nJets min: " << parameters.nJetsCut << ", "
+        << "HT Cut: " << parameters.HTCut << ", "
+        << "nElectrons: " << parameters.nElectronsCut << ", "
+        << "nMuons: " << parameters.nMuonsCut << ", "
+        << "nPhotonsWithNeutralinoMom: " << parameters.nPhotonsWithNeutralinoMom;
+    return out;
+  }
 };
 
+enum class PhotonSelectionType{medium, fake, mediumfake};
+std::string getPhotonSelectionTypeString(PhotonSelectionType type) {
+  std::string outputString;
+  switch(type) {
+  case (PhotonSelectionType::medium) :
+    outputString = "PhotonSelectionType::medium";
+    break;
+  case (PhotonSelectionType::fake) :
+    outputString = "PhotonSelectionType::fake";
+    break;
+  case (PhotonSelectionType::mediumfake) :
+    outputString = "PhotonSelectionType::mediumfake";
+    break;
+  default:
+    std::cout << "ERROR: Unknown photon selection type!"<< std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+  return outputString;
+}
+
 struct optionsStruct {
-  std::string inputFilePath, outputFilePath, photonSelectionType;
+  std::string inputFilesList, outputFilePath;
+  bool isMC;
+  PhotonSelectionType photonSelectionType;
   long counterStartInclusive, counterEndInclusive;
   int year, JECUncertainty;
+
+  friend std::ostream& operator<< (std::ostream& out, const optionsStruct& options) {
+    out << "inputFilesList: " << options.inputFilesList << std::endl
+        << "outputFilePath: " << options.outputFilePath << std::endl
+        << "isMC: " << (options.isMC? "true": "false") << ", "
+        << "photonSelectionType: " << getPhotonSelectionTypeString(options.photonSelectionType) << std::endl
+        << "Event range: [" << options.counterStartInclusive << ", " << options.counterEndInclusive << "]" << std::endl
+        << "year: " << options.year << ", "
+        << "JEC Uncertainty scale: " << options.JECUncertainty;
+    return out;
+  }
+};
+
+enum class photonFailureCategory{eta=0, pT, hOverE, neutralIsolation, photonIsolation, conversionSafeElectronVeto, R9, sigmaietaiataXORchargedIsolation, mediumIDCut, nPhotonFailureCategories};
+int photonFailureCategoryFirst = static_cast<int>(photonFailureCategory::eta);
+std::map<photonFailureCategory, std::string> photonFailureCategoryNames = {
+  {photonFailureCategory::eta, "eta"},
+  {photonFailureCategory::pT, "pT"},
+  {photonFailureCategory::hOverE, "hOverE"},
+  {photonFailureCategory::neutralIsolation, "neutralIsolation"},
+  {photonFailureCategory::photonIsolation, "photonIsolation"},
+  {photonFailureCategory::conversionSafeElectronVeto, "conversionSafeElectronVeto"},
+  {photonFailureCategory::R9, "R9"},
+  {photonFailureCategory::sigmaietaiataXORchargedIsolation, "sigmaietaiataXORchargedIsolation"},
+  {photonFailureCategory::mediumIDCut, "mediumIDCut"}
+};
+
+enum class jetFailureCategory{eta=0, pT, PFLooseID, puID, jetID, nJetFailureCategories};
+int jetFailureCategoryFirst = static_cast<int>(jetFailureCategory::eta);
+std::map<jetFailureCategory, std::string> jetFailureCategoryNames = {
+  {jetFailureCategory::eta, "eta"},
+  {jetFailureCategory::pT, "pT"},
+  {jetFailureCategory::PFLooseID, "PFLooseID"},
+  {jetFailureCategory::puID, "puID"},
+  {jetFailureCategory::jetID, "jetID"}
+};
+
+enum class eventFailureCategory{HLTPhoton=0, wrongNMediumOrFakePhotons, wrongNPhotons, HLTJet, wrongNJets, hTCut, electronVeto, muonVeto, MCGenInformation, nEventFailureCategories};
+int eventFailureCategoryFirst = static_cast<int>(eventFailureCategory::HLTPhoton);
+std::map<eventFailureCategory, std::string> eventFailureCategoryNames = {
+  {eventFailureCategory::HLTPhoton, "HLTPhoton"},
+  {eventFailureCategory::wrongNMediumOrFakePhotons, "wrongNMediumOrFakePhotons"},
+  {eventFailureCategory::wrongNPhotons, "wrongNPhotons"},
+  {eventFailureCategory::HLTJet, "HLTJet"},
+  {eventFailureCategory::wrongNJets, "wrongNJets"},
+  {eventFailureCategory::hTCut, "hTCut"},
+  {eventFailureCategory::electronVeto, "electronVeto"},
+  {eventFailureCategory::muonVeto, "muonVeto"},
+  {eventFailureCategory::MCGenInformation, "MCGenInformation"}
+};
+
+enum class miscCounter{failingPhotons=0, failingJets, failingEvents, acceptedEvents, nMiscCounters};
+int miscCounterFirst = static_cast<int>(miscCounter::failingPhotons);
+std::map<miscCounter, std::string> miscCounterNames = {
+  {miscCounter::failingPhotons, "failingPhotons"},
+  {miscCounter::failingJets, "failingJets"},
+  {miscCounter::failingEvents, "failingEvents"},
+  {miscCounter::acceptedEvents, "acceptedEvents"}
+};
+
+struct countersStruct{
+  std::map<photonFailureCategory, long> photonFailureCounters;
+  std::map<jetFailureCategory, long> jetFailureCounters;
+  std::map<eventFailureCategory, long> eventFailureCounters;
+  std::map<miscCounter, long> miscCounters;
 };
--- eventSelection/src/runEventSelection.cpp
+++ eventSelection/src/runEventSelection.cpp
@@ -2,30 +2,173 @@
 
 optionsStruct getOptionsFromParser(tmArgumentParser& argumentParser) {
   optionsStruct options = optionsStruct();
-  // options.inputMCPath = argumentParser.getArgumentString("inputMCPath");
-  options.inputFilePath = argumentParser.getArgumentString("inputFilePath");
+  options.inputFilesList = argumentParser.getArgumentString("inputFilesList");
   options.outputFilePath = argumentParser.getArgumentString("outputFilePath");
+  std::string MCString = argumentParser.getArgumentString("isMC");
+  if (MCString == "true") {
+    options.isMC = true;
+  }
+  else if (MCString == "false") {
+    options.isMC = false;
+  }
+  else {
+    std::cout << "ERROR: argument \"isMC\" can be either the string \"true\" or the string \"false\"; current value: " << MCString << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   options.counterStartInclusive = std::stol(argumentParser.getArgumentString("counterStartInclusive"));
   options.counterEndInclusive = std::stol(argumentParser.getArgumentString("counterEndInclusive"));
-  options.photonSelectionType = argumentParser.getArgumentString("photonSelectionType");
+  std::string photonSelectionTypeString = argumentParser.getArgumentString("photonSelectionType");
+  if (photonSelectionTypeString == "medium") {
+    options.photonSelectionType = PhotonSelectionType::medium;
+  }
+  else if (photonSelectionTypeString == "fake") {
+    options.photonSelectionType = PhotonSelectionType::fake;
+  }
+  else if (photonSelectionTypeString == "mediumfake") {
+    options.photonSelectionType = PhotonSelectionType::mediumfake;
+  }
+  else {
+    std::cout << "ERROR: argument \"photonSelectionType\" can be one of \"medium\", \"fake\", or \"mediumfake\"; current value: " << photonSelectionTypeString << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   options.year = std::stoi(argumentParser.getArgumentString("year"));
+  if (!(options.year == 2016 || options.year == 2017 || options.year == -1)) {
+    std::cout << "ERROR: argument \"year\" can be one of 2016, 2017, or -1; current value: " << options.year << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   options.JECUncertainty = std::stoi(argumentParser.getArgumentString("JECUncertainty"));
+  if (std::abs(options.JECUncertainty) > 1) {
+    std::cout << "ERROR: argument \"JECUncertainty\" can be one of -1, 0, or +1; current value: " << options.JECUncertainty << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
   return options;
+}
+
+std::string getNDashes(const int& n) {
+  std::stringstream dashes;
+  for (int counter = 0; counter < n; ++counter) dashes << "-";
+  return dashes.str();
+}
+
+void initializeCounters(countersStruct &counters) {
+  for (int categoryIndex = photonFailureCategoryFirst; categoryIndex != static_cast<int>(photonFailureCategory::nPhotonFailureCategories); ++categoryIndex) {
+    photonFailureCategory category = static_cast<photonFailureCategory>(categoryIndex);
+    counters.photonFailureCounters[category] = 0l;
+  }
+
+  for (int categoryIndex = jetFailureCategoryFirst; categoryIndex != static_cast<int>(jetFailureCategory::nJetFailureCategories); ++categoryIndex) {
+    jetFailureCategory category = static_cast<jetFailureCategory>(categoryIndex);
+    counters.jetFailureCounters[category] = 0l;
+  }
+
+  for (int categoryIndex = eventFailureCategoryFirst; categoryIndex != static_cast<int>(eventFailureCategory::nEventFailureCategories); ++categoryIndex) {
+    eventFailureCategory category = static_cast<eventFailureCategory>(categoryIndex);
+    counters.eventFailureCounters[category] = 0l;
+  }
+
+  for (int miscCounterIndex = miscCounterFirst; miscCounterIndex != static_cast<int>(miscCounter::nMiscCounters); ++miscCounterIndex) {
+    miscCounter miscCounterEnumIndex = static_cast<miscCounter>(miscCounterIndex);
+    counters.miscCounters[miscCounterEnumIndex] = 0l;
+  }
+}
+
+void printCounters(countersStruct &counters) {
+  std::cout << "Photon counters: " << std::endl;
+  for (const auto& counterValuePair : counters.photonFailureCounters) {
+    std::cout << photonFailureCategoryNames[counterValuePair.first] << " : " << counterValuePair.second << std::endl;
+  }
+  std::cout << getNDashes(100) << std::endl;
+
+  std::cout << "Jet counters: " << std::endl;
+  for (const auto& counterValuePair : counters.jetFailureCounters) {
+    std::cout << jetFailureCategoryNames[counterValuePair.first] << " : " << counterValuePair.second << std::endl;
+  }
+  std::cout << getNDashes(100) << std::endl;
+
+  std::cout << "Event counters: " << std::endl;
+  for (const auto& counterValuePair : counters.eventFailureCounters) {
+    std::cout << eventFailureCategoryNames[counterValuePair.first] << " : " << counterValuePair.second << std::endl;
+  }
+  std::cout << getNDashes(100) << std::endl;
+
+  std::cout << "Miscellaneous counters: " << std::endl;
+  for (const auto& counterValuePair : counters.miscCounters) {
+    std::cout << miscCounterNames[counterValuePair.first] << " : " << counterValuePair.second << std::endl;
+  }
+}
+
+void incrementCounters(photonFailureCategory photonCategory, countersStruct& counters) {
+  ++((counters.photonFailureCounters)[photonCategory]);
+}
+
+void incrementCounters(jetFailureCategory jetCategory, countersStruct& counters) {
+  ++((counters.jetFailureCounters)[jetCategory]);
+}
+
+void incrementCounters(eventFailureCategory eventCategory, countersStruct& counters) {
+  ++((counters.eventFailureCounters)[eventCategory]);
+}
+
+void incrementCounters(miscCounter miscCounterEnumIndex, countersStruct& counters) {
+  ++((counters.miscCounters)[miscCounterEnumIndex]);
 }
 
 int main(int argc, char* argv[]) {
   gROOT->SetBatch();
-  parametersStruct parameters = parametersStruct();
   tmArgumentParser argumentParser = tmArgumentParser("Calculate systematics due to uncertainty on jet energy corrections.");
-  argumentParser.addArgument("inputFilePath", "", true, "Path to input file.");
+  argumentParser.addArgument("inputFilesList", "", true, "Path to file containing list of input files.");
   argumentParser.addArgument("outputFilePath", "", true, "Path to output file.");
+  argumentParser.addArgument("isMC", "false", false, "Input file is a MC sample -- disable HLT photon trigger and enable additional MC selection.");
   argumentParser.addArgument("counterStartInclusive", "", true, "Event number from input file from which to start. The event with this index is included in the processing.");
   argumentParser.addArgument("counterEndInclusive", "", true, "Event number from input file at which to end. The event with this index is included in the processing.");
   argumentParser.addArgument("photonSelectionType", "fake", true, "Photon selection type: can be any one of: \"fake\", \"medium\", \"mediumfake\", \"fakeMC\", \"mediumMC\", \"mediumfakeMC\"");
-  argumentParser.addArgument("year", "-1", false, "Year of data-taking. Affects the HLT photon Bit index in the format of the n-tuplizer on which to trigger, and the photon ID cuts which are based on year-dependent recommendations. Default year: -1, which is used for MC and means the trigger is disabled and the 2017 photon ID recommendations are implemented.");
-  argumentParser.addArgument("JECUncertainty", "0", false, "Apply a uniform upward or downward jet energy uncertainty correction to jet pt. Default: 0, i.e. do not apply any other correction. +/-1 are allowed as well, shifting all jet pt up or down respectively by the relevant jet energy correction.");
+  argumentParser.addArgument("year", "2017", false, "Year of data-taking. Affects the HLT photon Bit index in the format of the n-tuplizer on which to trigger (unless sample is MC), and the photon ID cuts which are based on year-dependent recommendations.");
+  argumentParser.addArgument("JECUncertainty", "0", false, "Apply a uniform upward or downward jet energy uncertainty correction to jet pt. Default: 0, i.e. do not apply any other correction. +/-1 are allowed as well, shifting all jet pt up or down respectively by 1.0 times the uncertainty on the jet energy correction.");
   argumentParser.setPassedStringValues(argc, argv);
+
   optionsStruct options = getOptionsFromParser(argumentParser);
-  parameters.setHLTPhotonBitForYear(options.year);
+
+  parametersStruct parameters = parametersStruct();
+  parameters.tuneParametersForYear(options.year);
+
+  countersStruct counters = countersStruct();
+  initializeCounters(counters);
+
+  std::stringstream optionsStringstream;
+  optionsStringstream << options;
+  TNamed *optionsObject = new TNamed("optionsString", optionsStringstream.str().c_str());
+  std::stringstream parametersStringstream;
+  parametersStringstream << parameters;
+  TNamed *parametersObject = new TNamed("parametersString", parametersStringstream.str().c_str());
+  TFile *outputFile = TFile::Open(options.outputFilePath.c_str(), "RECREATE");
+  if (!(outputFile->IsOpen()) || outputFile->IsZombie()) {
+    std::cout << "ERROR: Unable to open output file to write. File path: " << options.outputFilePath << std::endl;
+  }
+
+  outputFile->WriteTObject(parametersObject);
+  outputFile->WriteTObject(optionsObject);
+  outputFile->Close();
+
+  std::cout << getNDashes(100) << std::endl;
+  // For testing: first print counters, should have all zeros
+  std::cout << "Empty counters:" << std::endl;
+  printCounters(counters);
+  incrementCounters(photonFailureCategory::pT, counters);
+  incrementCounters(photonFailureCategory::pT, counters);
+  incrementCounters(jetFailureCategory::eta, counters);
+  incrementCounters(eventFailureCategory::MCGenInformation, counters);
+  incrementCounters(eventFailureCategory::MCGenInformation, counters);
+  incrementCounters(eventFailureCategory::MCGenInformation, counters);
+  incrementCounters(miscCounter::acceptedEvents, counters);
+  // Should now have 1 in these categories
+  printCounters(counters);
+  std::cout << getNDashes(100) << std::endl
+            << "Options:" << std::endl
+            << optionsStringstream.str() << std::endl
+            << getNDashes(100) << std::endl;
+  std::cout << getNDashes(100) << std::endl
+            << "Parameters:" << std::endl
+            << parametersStringstream.str() << std::endl
+            << getNDashes(100) << std::endl;
   return 0;
 }
