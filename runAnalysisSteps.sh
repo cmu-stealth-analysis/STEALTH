01779171ca9fb55c08d28b3451d931757dce9d74
Tanmay Mudholkar <tkmudholkar@gmail.com>
2018-09-20 12:18:53 +0200

    Changed analysis workflow for oldHLT_noInvMassCut.

--- runAnalysisSteps.sh
+++ runAnalysisSteps.sh
@@ -3,16 +3,17 @@
 cd /uscms/home/tmudholk/private/stealth/STEALTH && source setupEnv.sh && set -x
 
 COMMON_XROOT_PREFIX="root://cmseos.fnal.gov/"
-INPUTDATADIR_CONTROL="${COMMON_XROOT_PREFIX}/store/user/lpcsusystealth/selections/testsMerged/oldCode_newHLT/combinedControl"
-INPUTDATADIR_SIGNAL="${COMMON_XROOT_PREFIX}/store/user/lpcsusystealth/selections/testsMerged/oldCode_newHLT/combinedSignal"
+INPUTDATADIR_CONTROL="${COMMON_XROOT_PREFIX}/store/user/lpcsusystealth/selections/testsMerged/oldHLT_noInvMassCut/combinedControl"
+INPUTDATADIR_SIGNAL="${COMMON_XROOT_PREFIX}/store/user/lpcsusystealth/selections/testsMerged/oldHLT_noInvMassCut/combinedSignal"
+INPUTDATADIR_MCSIGNAL="${COMMON_XROOT_PREFIX}/store/user/lpcsusystealth/selections/combinedSignal"
 
 INTEGLUMI=""
 INTEGLUMI_FRACTIONALERROR=""
 DATAPATTERNCONTROL=""
 DATAPATTERNSIGNAL=""
-MCPATTERNSIGNAL="MC_2018Production_DoubleMedium_oldCode_newHLT.root"
-MCPATTERNSIGNAL_JECUP="MC_2018Production_JECUp_DoubleMedium_oldCode_newHLT.root"
-MCPATTERNSIGNAL_JECDOWN="MC_2018Production_JECDown_DoubleMedium_oldCode_newHLT.root"
+MCPATTERNSIGNAL="MC_2018Production_DoubleMedium.root"
+MCPATTERNSIGNAL_JECUP="MC_2018Production_DoubleMedium_JECUp.root"
+MCPATTERNSIGNAL_JECDOWN="MC_2018Production_DoubleMedium_JECDown.root"
 MCLUMIYEARIDENTIFIER=""
 YEARIDENTIFIER=""
 
@@ -24,24 +25,24 @@ case ${2} in
     2016)
         INTEGLUMI="37760.0"
         INTEGLUMI_FRACTIONALERROR="0.025"
-        DATAPATTERNCONTROL="data_DoubleEG_2016_*_oldCode_newHLT.root"
-        DATAPATTERNSIGNAL="data_DoubleEG_2016_DoubleMedium_oldCode_newHLT.root"
+        DATAPATTERNCONTROL="data_DoubleEG_2016_*_oldHLT_noInvMassCut.root"
+        DATAPATTERNSIGNAL="data_DoubleEG_2016_DoubleMedium_oldHLT_noInvMassCut.root"
         MCLUMIYEARIDENTIFIER="_lumi2016"
         YEARIDENTIFIER="_2016"
         ;;
     2017)
         INTEGLUMI="46020.0"
         INTEGLUMI_FRACTIONALERROR="0.023"
-        DATAPATTERNCONTROL="data_DoubleEG_2017_*_oldCode_newHLT.root"
-        DATAPATTERNSIGNAL="data_DoubleEG_2017_DoubleMedium_oldCode_newHLT.root"
+        DATAPATTERNCONTROL="data_DoubleEG_2017_*_oldHLT_noInvMassCut.root"
+        DATAPATTERNSIGNAL="data_DoubleEG_2017_DoubleMedium_oldHLT_noInvMassCut.root"
         MCLUMIYEARIDENTIFIER="_lumi2017"
         YEARIDENTIFIER="_2017"
         ;;
     2016Plus2017)
         INTEGLUMI="83780.0"
         INTEGLUMI_FRACTIONALERROR="0.024"
-        DATAPATTERNCONTROL="data_DoubleEG_201*_oldCode_newHLT.root"
-        DATAPATTERNSIGNAL="data_DoubleEG_201*_DoubleMedium_oldCode_newHLT.root"
+        DATAPATTERNCONTROL="data_DoubleEG_201*_oldHLT_noInvMassCut.root"
+        DATAPATTERNSIGNAL="data_DoubleEG_201*_DoubleMedium_oldHLT_noInvMassCut.root"
         ;;
     *)
         echo "Unrecognized or empty year: ${2}"
@@ -57,7 +58,7 @@ case ${1} in
         ./getDataEventHistogramsAndSystematics.py --inputFilePath "${INPUTDATADIR_SIGNAL}/${DATAPATTERNSIGNAL}" --outputPrefix signal${YEARIDENTIFIER} --isSignal
         ;;
     3)
-        ./getMCSystematics/bin/getEventHistograms inputMCPath=${INPUTDATADIR_SIGNAL}/${MCPATTERNSIGNAL} inputMCPath_JECUp=${INPUTDATADIR_SIGNAL}/${MCPATTERNSIGNAL_JECUP} inputMCPath_JECDown=${INPUTDATADIR_SIGNAL}/${MCPATTERNSIGNAL_JECDOWN} outputPrefix=MC_2018${MCLUMIYEARIDENTIFIER} integratedLuminosity=${INTEGLUMI}
+        ./getMCSystematics/bin/getEventHistograms inputMCPath=${INPUTDATADIR_MCSIGNAL}/${MCPATTERNSIGNAL} inputMCPath_JECUp=${INPUTDATADIR_MCSIGNAL}/${MCPATTERNSIGNAL_JECUP} inputMCPath_JECDown=${INPUTDATADIR_MCSIGNAL}/${MCPATTERNSIGNAL_JECDOWN} outputPrefix=MC_2018${MCLUMIYEARIDENTIFIER} integratedLuminosity=${INTEGLUMI}
         ;;
     4)
         ./getMCSystematics/bin/getMCUncertainties inputPath=analysis/MCEventHistograms/MC_2018${MCLUMIYEARIDENTIFIER}_savedObjects.root outputPrefix=MC_2018${MCLUMIYEARIDENTIFIER}
