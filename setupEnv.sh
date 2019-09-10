d632e2f84647410f46b0a65b5b15cfebb7db3526
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-09-10 13:40:09 -0500

    Updated CMSSW version in environment.

--- setupEnv.sh
+++ setupEnv.sh
@@ -7,7 +7,7 @@ if [[ "$BASEHOSTNAME" =~ ^lxplus[0-9]{3,4}$ ]]; then
     export X509_USER_PROXY=/afs/cern.ch/user/t/tmudholk/private/x509up_u83667
     set +x && source /cvmfs/cms.cern.ch/cmsset_default.sh && set -x
     export SCRAM_ARCH=slc6_amd64_gcc630
-    cd /afs/cern.ch/user/t/tmudholk/public/research/stealth/from_michael/StealthProduction/CMSSW_9_4_9_cand2/src && set +x && eval `scramv1 runtime -sh` && export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH && set -x
+    cd /afs/cern.ch/user/t/tmudholk/public/research/stealth/from_michael/StealthProduction/CMSSW_9_4_13/src && set +x && eval `scramv1 runtime -sh` && export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH && set -x
     echo "Set default CMSSW_BASE = ${CMSSW_BASE}"
     cd ${WORKDIR}
 elif [[ "$BASEHOSTNAME" =~ ^cmslpc[0-9]{2}$ ]]; then
@@ -15,7 +15,7 @@ elif [[ "$BASEHOSTNAME" =~ ^cmslpc[0-9]{2}$ ]]; then
     export X509_USER_PROXY=/uscms/home/tmudholk/private/x509up_u50838
     set +x && source /cvmfs/cms.cern.ch/cmsset_default.sh && set -x
     export SCRAM_ARCH=slc6_amd64_gcc630
-    cd /uscms/home/tmudholk/private/stealth/cmssw/CMSSW_9_4_9_cand2/src && set +x && eval `scramv1 runtime -sh` && export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH && set -x
+    cd /uscms/home/tmudholk/private/stealth/cmssw/CMSSW_9_4_13/src && set +x && eval `scramv1 runtime -sh` && export PYTHONPATH=/afs/cern.ch/user/t/tmudholk/public/tmPyUtils:$PYTHONPATH && set -x
     echo "Set default CMSSW_BASE = ${CMSSW_BASE}"
     cd ${WORKDIR}
 else
