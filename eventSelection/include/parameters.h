bbf84f4a2cc364d386d6a4a3d2da3676c9f089cb
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-09-27 20:14:24 -0500

    Added HLT emulation for high-PT double photon trigger.

--- eventSelection/include/parameters.h
+++ eventSelection/include/parameters.h
@@ -56,8 +56,7 @@ struct parametersStruct {
       /* 37: HLT_Diphoton30PV_18PV_R9Id_AND_IsoCaloId_AND_HE_R9Id_PixelVeto_Mass55_v */
       /* 22: HLT_DoublePhoton70_v */
 
-      if (isMC) HLTPhotonBit = -1;
-      else HLTPhotonBit = 37;
+      HLTPhotonBit = 37;
       invariantMassCut = 60.0f;
 
       photonQualityCutsBarrel = photonQualityCutsStruct(0.02197f, 0.04596f, 0.01015f, 0.0106f, 1.141f, 1.694f, 1.189f, 0.01512f, 0.00002259f, 24.032f, 0.01512f, 0.00002259f, 2.080f, 0.004017f, 2.876f, 0.004017f);
@@ -98,8 +97,7 @@ struct parametersStruct {
       }
     }
     else if (year == 2016) {
-      if (isMC) HLTPhotonBit = -1;
-      else HLTPhotonBit = 16;
+      HLTPhotonBit = 16;
       invariantMassCut = 60.0f;
 
       photonQualityCutsBarrel = photonQualityCutsStruct(0.0396f, 0.0597f, 0.01022f, 0.01031f, 0.441f, 1.295f, 2.725f, 0.0148f, 0.000017f, 10.91f, 0.0148f, 0.000017f, 2.571f, 0.0047f, 3.630f, 0.0047f);
