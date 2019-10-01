91771c340f1a3003f9664cb7358877bc76d2e633
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-10-01 09:55:12 -0500

    Modified script to plot MC generated masses -- each file is now opened just before its events are analyzed.

--- plotGeneratedMassesInMC.py
+++ plotGeneratedMassesInMC.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import ROOT, tmROOTUtils, sys, argparse
+import ROOT, tmROOTUtils, sys, argparse, os
 from tmProgressBar import tmProgressBar
 
 ROOT.gROOT.SetBatch(ROOT.kTRUE)
@@ -18,6 +18,8 @@ inputArgumentsParser.add_argument('--minNeutralinoMass', default=93.75, help='Mi
 inputArgumentsParser.add_argument('--maxNeutralinoMass', default=2356.25, help='Max neutralino mass.',type=float)
 inputArguments = inputArgumentsParser.parse_args()
 
+os.system("mkdir -p {oF}".format(oF=inputArguments.outputFolder))
+
 MCPIDs = {
     "photon": 22,
     "gluino": 1000021,
@@ -31,20 +33,62 @@ h_gluinoMass = ROOT.TH1F("h_gluinoMass", ";m_{#tilde{#it{g}}};Total nEvents", in
 h_neutralinoMass = ROOT.TH1F("h_neutralinoMass", ";m_{#tilde{#it{#chi_{1}^{0}}}};Total nEvents", inputArguments.nNeutralinoMassBins, inputArguments.minNeutralinoMass, inputArguments.maxNeutralinoMass)
 h_masses = ROOT.TH2F("h_masses", "Total nEvents;m_{#tilde{#it{g}}};m_{#tilde{#it{#chi_{1}^{0}}}}", inputArguments.nGluinoMassBins, inputArguments.minGluinoMass, inputArguments.maxGluinoMass, inputArguments.nNeutralinoMassBins, inputArguments.minNeutralinoMass, inputArguments.maxNeutralinoMass)
 
+def fillHistogramsFromFile(fileName, histGluinoMass, histNeutralinoMass, hist2D, randomNumberGenerator):
+    global nEvents_neutralinoMassUnset, nEvents_analyzed, minGluinoMassFound, maxGluinoMassFound, minNeutralinoMassFound, maxNeutralinoMassFound
+    inputMCChain = ROOT.TChain('ggNtuplizer/EventTree')
+    inputMCChain.Add(fileName)
+    nMCEntries = inputMCChain.GetEntries()
+    print ("Number of events in file {f}: {nMCEntries}".format(f=fileName, nMCEntries=nMCEntries))
+    for entryIndex in range(nMCEntries):
+        if prescaleEnabled:
+            randomNumber = randomNumberGenerator.Uniform()
+            if randomNumber > prescaleInverse: continue
+        nEvents_analyzed += 1
+        entryStatus = inputMCChain.LoadTree(entryIndex)
+        if entryStatus < 0: sys.exit("Tree failed to load entry at index {entryIndex}; returned status {entryStatus}".format(entryIndex=entryIndex, entryStatus=entryStatus))
+        chainStatus = inputMCChain.GetEntry(entryIndex)
+        if chainStatus <= 0: sys.exit("Unable to load MC from chain at index {entryIndex}; GetEntry returned status {chainStatus}".format(entryIndex=entryIndex, chainStatus=chainStatus))
+        generatedMasses = {"gluino": 0., "neutralino": 0.}
+        gluinoMassIsSet = False
+        neutralinoMassIsSet = False
+        for genParticleIndex in range(inputMCChain.nMC):
+            if (not(gluinoMassIsSet) and inputMCChain.mcPID[genParticleIndex] == MCPIDs["gluino"]):
+                generatedMasses["gluino"] = inputMCChain.mcMass[genParticleIndex]
+                gluinoMassIsSet = True
+            if (not(neutralinoMassIsSet) and inputMCChain.mcMomPID[genParticleIndex] == MCPIDs["neutralino"]):
+                generatedMasses["neutralino"] = inputMCChain.mcMomMass[genParticleIndex]
+                neutralinoMassIsSet = True
+            if (gluinoMassIsSet and neutralinoMassIsSet): break
+        if gluinoMassIsSet:
+            h_gluinoMass.Fill(generatedMasses["gluino"])
+        else:
+            sys.exit("Gluino mass unset in event with index {index}".format(index=entryIndex))
+        if neutralinoMassIsSet:
+            h_neutralinoMass.Fill(generatedMasses["neutralino"])
+            h_masses.Fill(generatedMasses["gluino"], generatedMasses["neutralino"])
+        else:
+            nEvents_neutralinoMassUnset += 1 # sys.exit("Unable to find neutralino mass in an event!")
+        if ((minGluinoMassFound < 0) or (generatedMasses["gluino"] < minGluinoMassFound)):
+            minGluinoMassFound = generatedMasses["gluino"]
+        if ((maxGluinoMassFound < 0) or (generatedMasses["gluino"] > maxGluinoMassFound)):
+            maxGluinoMassFound = generatedMasses["gluino"]
+        if ((minNeutralinoMassFound < 0) or (generatedMasses["neutralino"] < minNeutralinoMassFound)):
+            minNeutralinoMassFound = generatedMasses["neutralino"]
+        if ((maxNeutralinoMassFound < 0) or (generatedMasses["neutralino"] > maxNeutralinoMassFound)):
+            maxNeutralinoMassFound = generatedMasses["neutralino"]
+        if ((generatedMasses["gluino"] < inputArguments.minGluinoMass) or
+            (generatedMasses["gluino"] > inputArguments.maxGluinoMass) or
+            (generatedMasses["neutralino"] > inputArguments.maxNeutralinoMass)):
+            sys.exit("ERROR: Unexpected (gluino, neutralino) masses: {g,n}".format(g=generatedMasses["gluino"], n=generatedMasses["neutralino"]))
+
+randomNumberGenerator = ROOT.TRandom2()
+randomNumberGenerator.SetSeed(100)
+
 listOfInputFiles = []
 inputFileNamesFileObject = open(inputArguments.inputFilesList, 'r')
 for inputFileName in inputFileNamesFileObject:
     listOfInputFiles.append(inputFileName.strip())
 inputFileNamesFileObject.close()
-
-inputMCChain = ROOT.TChain('ggNtuplizer/EventTree')
-for inputFile in listOfInputFiles:
-    inputMCChain.Add(inputFile)
-nMCEntries = inputMCChain.GetEntries()
-print ("Total number of available events in MC: {nMCEntries}".format(nMCEntries=nMCEntries))
-
-randomNumberGenerator = ROOT.TRandom2()
-randomNumberGenerator.SetSeed(100)
 
 nEvents_neutralinoMassUnset = 0
 nEvents_analyzed = 0
@@ -53,52 +97,8 @@ maxGluinoMassFound = -1
 minNeutralinoMassFound = -1
 maxNeutralinoMassFound = -1
 
-progressBar = tmProgressBar(nMCEntries)
-progressBarUpdatePeriod = max(1, nMCEntries//1000)
-progressBar.initializeTimer()
-for entryIndex in range(nMCEntries):
-    if (entryIndex%progressBarUpdatePeriod == 0): progressBar.updateBar(1.0*entryIndex/nMCEntries, entryIndex)
-    if prescaleEnabled:
-        randomNumber = randomNumberGenerator.Uniform()
-        if randomNumber > prescaleInverse: continue
-    nEvents_analyzed += 1
-    entryStatus = inputMCChain.LoadTree(entryIndex)
-    if entryStatus < 0: sys.exit("Tree failed to load entry at index {entryIndex}; returned status {entryStatus}".format(entryIndex=entryIndex, entryStatus=entryStatus))
-    chainStatus = inputMCChain.GetEntry(entryIndex)
-    if chainStatus <= 0: sys.exit("Unable to load MC from chain at index {entryIndex}; GetEntry returned status {chainStatus}".format(entryIndex=entryIndex, chainStatus=chainStatus))
-    generatedMasses = {"gluino": 0., "neutralino": 0.}
-    gluinoMassIsSet = False
-    neutralinoMassIsSet = False
-    for genParticleIndex in range(inputMCChain.nMC):
-        if (not(gluinoMassIsSet) and inputMCChain.mcPID[genParticleIndex] == MCPIDs["gluino"]):
-            generatedMasses["gluino"] = inputMCChain.mcMass[genParticleIndex]
-            gluinoMassIsSet = True
-        if (not(neutralinoMassIsSet) and inputMCChain.mcMomPID[genParticleIndex] == MCPIDs["neutralino"]):
-            generatedMasses["neutralino"] = inputMCChain.mcMomMass[genParticleIndex]
-            neutralinoMassIsSet = True
-        if (gluinoMassIsSet and neutralinoMassIsSet): break
-    if gluinoMassIsSet:
-        h_gluinoMass.Fill(generatedMasses["gluino"])
-    else:
-        sys.exit("Gluino mass unset in event with index {index}".format(index=entryIndex))
-    if neutralinoMassIsSet:
-        h_neutralinoMass.Fill(generatedMasses["neutralino"])
-        h_masses.Fill(generatedMasses["gluino"], generatedMasses["neutralino"])
-    else:
-        nEvents_neutralinoMassUnset += 1 # sys.exit("Unable to find neutralino mass in an event!")
-    if ((minGluinoMassFound < 0) or (generatedMasses["gluino"] < minGluinoMassFound)):
-        minGluinoMassFound = generatedMasses["gluino"]
-    if ((maxGluinoMassFound < 0) or (generatedMasses["gluino"] > maxGluinoMassFound)):
-        maxGluinoMassFound = generatedMasses["gluino"]
-    if ((minNeutralinoMassFound < 0) or (generatedMasses["neutralino"] < minNeutralinoMassFound)):
-        minNeutralinoMassFound = generatedMasses["neutralino"]
-    if ((maxNeutralinoMassFound < 0) or (generatedMasses["neutralino"] > maxNeutralinoMassFound)):
-        maxNeutralinoMassFound = generatedMasses["neutralino"]
-    if ((generatedMasses["gluino"] < inputArguments.minGluinoMass) or
-        (generatedMasses["gluino"] > inputArguments.maxGluinoMass) or
-        (generatedMasses["neutralino"] > inputArguments.maxNeutralinoMass)):
-        sys.exit("ERROR: Unexpected (gluino, neutralino) masses: {g,n}".format(g=generatedMasses["gluino"], n=generatedMasses["neutralino"]))
-progressBar.terminate()
+for fname in listOfInputFiles:
+    fillHistogramsFromFile(fname, h_gluinoMass, h_neutralinoMass, h_masses, randomNumberGenerator)
 
 print(("Min gluino mass found: {minGMF}, Max gluino mass found: {maxGMF}, Min neutralino mass found: {minNMF}, Max neutralino mass found: {maxNMF}").format(minGMF=minGluinoMassFound, maxGMF=maxGluinoMassFound, minNMF=minNeutralinoMassFound, maxNMF=maxNeutralinoMassFound))
 
