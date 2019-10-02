b0b5a4074cb21b914b3f0facc8af5af24822b845
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-10-02 14:39:16 -0500

    Added helper scripts to generate plot of generated event distribution in (mgluino, mneutralino) parameter-space.

---
+++ miscUtils/plotGeneratedMassesInMCHelper.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function xrdmv_with_check {
+    if [ "${#}" != 2  ]; then
+        echo "ERROR: number of arguments passed to \"${FUNCNAME}\": ${#}"
+        exit 1
+    fi
+    xrdcp -f ${1} ${2} 2>&1
+    XRDEXIT=${?}
+    if [[ ${XRDEXIT} -ne 0 ]]; then
+        rm -f *.root
+        echo "exit code ${XRDEXIT}, failure in xrdcp"
+        exit ${XRDEXIT}
+    fi
+    rm ${1}
+}
+
+cd ${_CONDOR_SCRATCH_DIR}
+
+# Make sure there's exactly one file beginning with "x509"
+x509Matches=`find . -maxdepth 2 -type f -name x509*`
+NMatchesMinusOne=`echo ${x509Matches} | tr -cd " \t" | wc -c`
+if [ "${NMatchesMinusOne}" != "0" ]; then
+    echo "ERROR: More than one file found beginning with x509"
+    exit 1
+fi
+
+# Move file to proxy
+mkdir proxy
+mv -v x509* proxy/
+x509Matches=`find proxy/ -maxdepth 2 -type f -name x509*`
+export X509_USER_PROXY=${_CONDOR_SCRATCH_DIR}/${x509Matches}
+echo "Set X509_USER_PROXY=${X509_USER_PROXY}"
+echo "voms output:"
+voms-proxy-info --all
+
+cd ${_CONDOR_SCRATCH_DIR}
+
+# Source CMSSW environment
+echo "Sourcing CMSSW environment..."
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc630
+eval `scramv1 project CMSSW CMSSW_9_4_13`
+cd CMSSW_9_4_13/src/
+eval `scramv1 runtime -sh` # cmsenv is not an alias on the workers
+echo "CMSSW: "${CMSSW_BASE}
+cd ${_CONDOR_SCRATCH_DIR}
+
+echo "Extracting tmUtils tarball..."
+./extract_tmUtilsTarball.sh && rm -f tmUtils.tar.gz
+
+echo "Setting env variables..."
+export PYTHONPATH=${_CONDOR_SCRATCH_DIR}/tmPyUtils:${PYTHONPATH}
+export TMPYUTILS=${_CONDOR_SCRATCH_DIR}/tmPyUtils/
+export TMCPPUTILS=${_CONDOR_SCRATCH_DIR}/tmCPPUtils/
+
+cd ${_CONDOR_SCRATCH_DIR}
+echo "Current directory structure: (excluding CMSSW)"
+ls -I "CMSSW*" -R
+
+set -x
+echo "PWD=${PWD}" && ./plotGeneratedMassesInMC.py --inputFilesList "inputFileList_MC_Fall17_stealth_t5Wg.txt" --outputPrefix mc_Fall17_stealth_t5Wg
+SCRIPT_STATUS="${?}"
+if [ "${SCRIPT_STATUS}" != "0" ]; then
+    echo "Error in script: exit with code ${SCRIPT_STATUS}"
+    exit ${SCRIPT_STATUS}
+fi
+
+echo "Copying output..."
+for fname in MCGeneratedMasses/*; do xrdmv_with_check ${fname} ${1}/${fname}; done
+echo "All done!"
+set +x
