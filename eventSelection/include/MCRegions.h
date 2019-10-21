85f8bfc7e1110b76257adfefa7e15c485b738c8f
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-10-21 14:33:25 -0500

    Added new MC regions of interest in the bulk: well-excluded zone, close to the expected limit contours, and not excluded.

--- eventSelection/include/MCRegions.h
+++ eventSelection/include/MCRegions.h
@@ -7,17 +7,23 @@
 
 namespace MCRegions {
   std::map<int, std::string> regionNames = {
-    {1, "bulk"},
-    {2, "lowNeutralinoMass"},
-    {3, "gluinoNeutralinoDegenerate"}
+    {1, "bulk_wellExcluded"},
+    {2, "bulk_closeToContours"},
+    {3, "bulk_notExcluded"},
+    {4, "lowNeutralinoMass"},
+    {5, "gluinoNeutralinoDegenerate"}
   };
 
   int getRegionIndex(const float& generated_gluinoMass, const float& generated_neutralinoMass) {
     if (generated_gluinoMass > 1200.) {
-      if (generated_neutralinoMass < 150.) return 2;
+      if (generated_neutralinoMass < 150.) return 4;
       else if (generated_neutralinoMass > 250.) {
-        if ((generated_gluinoMass - generated_neutralinoMass) < 150.) return 3;
-        else if ((generated_gluinoMass - generated_neutralinoMass) > 250.) return 1;
+        if ((generated_gluinoMass - generated_neutralinoMass) < 150.) return 5;
+        else if ((generated_gluinoMass - generated_neutralinoMass) > 250.) {
+	  if (generated_gluinoMass < 2000) return 1;
+	  else if (generated_gluinoMass < 2200) return 2;
+	  else return 3;
+	}
       }
     }
     return 0;
