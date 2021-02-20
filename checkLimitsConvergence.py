4534c7eb6d5e6c824a14eb257ef8ca20f8c315d4
Tanmay Mudholkar <tkmudholkar@gmail.com>
2021-02-20 10:06:41 -0600

    checkLimitsConvergence.py: criteria for flagging limits as problematic now relaxed, was creating some convergence issues.

--- checkLimitsConvergence.py
+++ checkLimitsConvergence.py
@@ -8,11 +8,23 @@ inputArgumentsParser = argparse.ArgumentParser(description='Get signal contamina
 inputArgumentsParser.add_argument('--inputROOTFile', required=True, help='Name of input ROOT file containing observed and expected limits.',type=str)
 inputArguments = inputArgumentsParser.parse_args()
 
+ACCEPTABLE_LOWER_RATIO = 0.2
+ACCEPTABLE_UPPER_RATIO = 5.0
+UPPER_LIMIT_CHECK_RELAXATION_THRESHOLD = 0.1
+
 limitsConverge = True
 try:
     expectedUpperLimit, expectedUpperLimitOneSigmaDown, expectedUpperLimitOneSigmaUp, observedUpperLimit = commonFunctions.get_expected_and_observed_limits_from_combine_output(combineOutputFilePath=inputArguments.inputROOTFile)
-    if (abs((observedUpperLimit/expectedUpperLimit)-1.0) >= 0.2): # Observed limit deviates from expected limit by more than 20%
-        limitsConverge = False
+    ratiosToCheck = [observedUpperLimit/expectedUpperLimit, expectedUpperLimitOneSigmaDown/expectedUpperLimit, expectedUpperLimitOneSigmaUp/expectedUpperLimit]
+
+    # for low expected limit values, the combine tool sometimes seems to not converge... doesn't seem to matter if we're away from the expected limit contours
+    # the 50% and "one sigma up" expected limits still seem to be reasonable, so we can use them to check for convergence
+    if (expectedUpperLimit < UPPER_LIMIT_CHECK_RELAXATION_THRESHOLD): ratiosToCheck = [expectedUpperLimitOneSigmaUp/expectedUpperLimit]
+
+    for ratio in ratiosToCheck:
+        if ((ratio < ACCEPTABLE_LOWER_RATIO) or (ratio > ACCEPTABLE_UPPER_RATIO)):
+            limitsConverge = False
+            break
 except ValueError:
     limitsConverge = False
 
