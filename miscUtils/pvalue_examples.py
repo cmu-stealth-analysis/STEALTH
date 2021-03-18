6f4216f1eb870f9e27af24e876e79409acdf0c99
Tanmay Mudholkar <tkmudholkar@gmail.com>
2021-03-17 21:42:00 -0500

    Added chi2 distribution to toy script fstatistic_example.py.

--- miscUtils/pvalue_examples.py
+++ miscUtils/pvalue_examples.py
@@ -10,7 +10,7 @@ import stealthEnv
 ROOT.gROOT.SetBatch(ROOT.kTRUE)
 ROOT.TH1.AddDirectory(ROOT.kFALSE)
 
-outputDirectory = stealthEnv.analysisRoot + "/ftest"
+outputDirectory = stealthEnv.analysisRoot + "/pvalue_examples"
 if not(os.path.isdir(outputDirectory)): subprocess.check_call("mkdir -p {oD}".format(oD=outputDirectory), shell=True, executable="/bin/bash")
 
 nPoints = 1000
@@ -35,6 +35,32 @@ for xIndex in range(0, 1+nPoints):
     # print("At x = {x}, pdf_value: {p}, cdf_value: {c}".format(x=x, p=pdf_value, c=cdf_value))
 
 for graphToPlot in [f_distribution, f_cdf]:
+    outputCanvas = ROOT.TCanvas("c_" + graphToPlot.GetName(), "")
+    graphToPlot.Draw("AC")
+    ROOT.gPad.SetLogx()
+    outputCanvas.SaveAs(outputDirectory + "/" + graphToPlot.GetName() + ".pdf")
+
+nPoints = 1000
+xMin = 0.02
+xMax = 20.
+dof = 1
+chi2_distribution = ROOT.TGraph()
+chi2_distribution.SetName("chi2_dist")
+chi2_distribution.SetTitle("#chi^2-distribution with 1 dof;t;density")
+chi2_cdf = ROOT.TGraph()
+chi2_cdf.SetName("chi2_cdf")
+chi2_cdf.SetTitle("CDF for #chi^2-distribution with 1 dof;F;")
+
+for xIndex in range(0, 1+nPoints):
+    x = xMin*math.pow(10, (xIndex/nPoints)*math.log10(xMax/xMin))
+    # x = xMin + (xIndex/nPoints)*(xMax-xMin)
+    pdf_value = scipy.stats.chi2.pdf(x, 1)
+    chi2_distribution.SetPoint(xIndex, x, pdf_value)
+    cdf_value = scipy.stats.chi2.cdf(x, 1)
+    chi2_cdf.SetPoint(xIndex, x, cdf_value)
+    # print("At x = {x}, pdf_value: {p}, cdf_value: {c}".format(x=x, p=pdf_value, c=cdf_value))
+
+for graphToPlot in [chi2_distribution, chi2_cdf]:
     outputCanvas = ROOT.TCanvas("c_" + graphToPlot.GetName(), "")
     graphToPlot.Draw("AC")
     ROOT.gPad.SetLogx()
